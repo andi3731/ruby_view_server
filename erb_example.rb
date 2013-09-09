aef74f25e9e53c93a8f5fe0b8f841d84b5246b75
Andrei Glingeanu <andi3731@gmail.com>
2013-09-09 20:53:03 +0300

    added some erb examples

--- erb_example.rb
+++ erb_example.rb
@@ -3,3 +3,11 @@ require 'erb'
 x = 42
 template = ERB.new "The value of x is: <%= x %>"
 puts template.result(binding)
+
+x = [1,2,3,4]
+template = ERB.new "The value of x is: <%= x %>"
+puts template.result(binding)
+
+x = {:hello => "world"}
+template = ERB.new "The value of x is: <%= x %>"
+puts template.result(binding)
